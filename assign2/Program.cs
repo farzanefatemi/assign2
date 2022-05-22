50b605ec2eb9e4ceb7eef18290812eaca4ac8d7d
farzane <farzanefatemi22@yahoo.com>
2022-05-22 21:10:39 +0430

    second assinment

---
+++ assign2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Session1
+{
+    public static void Main(string[] args)
+    {
+
+        for (int rows = 1; rows <= 5; rows++)
+        {
+            for (int spaces = 1; spaces <= 5 - rows; spaces++)
+            {
+                Console.Write(" ");
+            }
+            for (int numbers = 1; numbers <= rows; numbers++)
+            {
+                Console.Write(numbers);
+            }
+            for (int num = rows - 1; num >= 1; num--)
+            {
+                Console.Write(num);
+            }
+            Console.Write("\n");
+        }
+    }
+}
+
